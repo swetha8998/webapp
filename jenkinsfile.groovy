7cde9f216f1c95dbcd1532f13ebad573df05e58a
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 22:12:22 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -11,7 +11,7 @@ node{
     stage("deploy"){
       timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
    sshagent(['3e61a85b-994b-47e6-9c94-78e83f54ba82']) {
-   sh "cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps"
+  sh "whoami"
 }
 
     }
