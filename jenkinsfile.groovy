45b5f47d16c55b99c151bec0c0d14ebee42304a4
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 19:48:05 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,8 @@ node{
    
     stage("deploy"){
       timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
-deploy adapters: [tomcat8(credentialsId: 'tomcat', path: '', url: 'http://192.168.56.23:8090/')], contextPath: '/var/lib/jenkins/workspace/webapp/target', war: '**/*.war'
+     cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps
+
     }
 }
     
