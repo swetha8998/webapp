84805149b04442253d6ccd91875fbc1ffeee063d
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 18:33:24 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,7 @@ node{
    
     stage("deploy"){
       timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
-         sh "scp /var/lib/jenkins/workspace/heloapp/target/maven-0.0.1-SNAPSHOT.war root@192.168.56.23://opt/tomcat/webapps"
+         sh "scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/heloapp/target/maven-0.0.1-SNAPSHOT.war root@192.168.56.23://opt/tomcat/webapps"
 
     }
     
