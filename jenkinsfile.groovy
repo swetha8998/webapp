7000b7fdc2e1289be830e361761b1f5bd7b05ce3
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 20:55:29 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,7 +10,7 @@ node{
    
     stage("deploy"){
       timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
-   sshagent(['']) {
+   sshagent(['3e61a85b-994b-47e6-9c94-78e83f54ba82']) {
    sh "scp -o StrictHostKeyChecking=no /var/lib/jenkins/workspace/webapp/target/ root@192.168.56.23:/opt/tomcat/webapps"
 }
 
