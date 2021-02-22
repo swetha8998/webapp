fec1fe86273954172babf10c4d5bba6b13bd5677
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 18:29:57 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,9 +9,9 @@ node{
     }
    
     stage("deploy"){
-        sshagent(['']) {
-     sh "scp /var/lib/jenkins/workspace/heloapp/target/maven-0.0.1-SNAPSHOT.war root@192.168.56.23://opt/tomcat/webapps"
-}
+      timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
+         sh "scp /var/lib/jenkins/workspace/heloapp/target/maven-0.0.1-SNAPSHOT.war root@192.168.56.23://opt/tomcat/webapps"
+
     }
     
 }
