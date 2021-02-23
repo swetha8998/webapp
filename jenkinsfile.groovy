983a7ee8cfc7336e0c8864a27277c6d2a2ae5113
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-23 16:25:18 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -30,17 +30,16 @@ else
     ps -ef |grep tomcat
 fi'''
      
-   }}
-
-    stage("backup"){
+   
       sh "mkdir /opt/backup"
       sh "cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/backup"
       sh " echo 'backup is created' "
       
-    }
-  stage("post deployement"){
+    
+
     sh "curl http://192.168.56.23:8090/simplewebapp/"
-  }
+   }
+    }
 
 }
     
