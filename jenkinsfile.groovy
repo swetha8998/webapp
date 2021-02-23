33c2f8e2e61e2b12204a6617c4450b6209d18523
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-23 17:34:05 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -12,20 +12,35 @@ node{
       timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
    sshagent(['3e61a85b-994b-47e6-9c94-78e83f54ba82']) {
   sh "whoami"
- 
+     sh '''if [ $(ps -ef | grep tomcat | wc -l) == 1 ]
+ then 
    
+   cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps
+   cd /opt/tomcat/bin
+   echo "inside if"
+   ./startup.sh
+   ps -ef |grep tomcat
    
+else
+   cd /opt/tomcat/bin
+   echo "inisde else"
+   ./shutdown.sh
+   cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps
+    ./startup.sh
+    ps -ef |grep tomcat
+fi'''
+     
+   }}
 
- sh "cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps"
-sh '''cd /opt/tomcat/bin
-./startup.sh'''
-  
-    
-
-  
-   }
+    stage("backup"){
+      sh "mkdir /opt/backup"
+      sh "cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/backup"
+      sh " echo 'backup is created' "
+      
     }
+  stage("post deployement"){
+    sh "curl http://192.168.56.23:8090/simplewebapp/"
+  }
 
 }
-    
 
