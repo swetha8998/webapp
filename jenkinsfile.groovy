f4b4c6d2b259bd5ab446af512defda1e0af845be
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-24 15:00:20 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -11,16 +11,17 @@ node{
     stage("deploy"){
       timeout(time: 15, unit: "MINUTES") {    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'}
    sshagent(['3e61a85b-994b-47e6-9c94-78e83f54ba82']) {
-    cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps
+    sh "cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/tomcat/webapps"
      
    }}
 
     stage("backup"){
-      sh "mkdir /opt/backup"
+      
       sh '''if [ -d "/opt/backup" ]
         then
           echo "backup already exist"
       else 
+        sh "mkdir /opt/backup"
         cp -p /var/lib/jenkins/workspace/webapp/target/simplewebapp.war /opt/backup"
       fi
        echo \'backup is created\' '''
